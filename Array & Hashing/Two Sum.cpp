7bbb3ea4132314156b6a0434f41c86077d7960b3
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-18 12:53:25 +0500

    Question 3 added

---
+++ Array & Hashing/Two Sum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //Brute Force
+        // for (int i=0; i<nums.size(); i++) {
+        //     for (int j=0; j<nums.size(); j++) {
+        //         if (i == j) 
+        //             continue;
+        //         if ((nums[i] + nums[j]) == target)
+        //             return {i, j};
+        //     }
+        // }
+        // return {};
+
+        //HashMap
+        // unordered_map<int, int> map;
+        // for (int i=0; i<nums.size(); i++) {
+        //     int diff = target - nums[i];
+        //     if (map.find(diff) != map.end()) {
+        //         return {map[diff], i};
+        //     }
+        //     map[nums[i]] = i;
+        // }
+
+        // return {};
+    }
+};
