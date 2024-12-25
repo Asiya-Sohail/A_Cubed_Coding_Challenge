ba5ae5e67568e1a6242b8a88847eeae9fa39be9d
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-26 01:11:14 +0500

    Question 10 added

---
+++ Two Pointers/Valid Palindrome.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // Two pointers Approach
+        // int left = 0, right = s.size() - 1;
+        // while (left < right) {
+        //     while (! isalnum(s[left]) && left < right) {
+        //         left++; 
+        //     }
+        //     while (! isalnum(s[right]) && left < right) {
+        //         right--;
+        //     }
+        //     if (tolower(s[left]) != tolower(s[right]))
+        //         return false;
+        //     else {
+        //         left++; right--;
+        //     }    
+        // }
+        // return true;
+
+        // Reverse Function approach
+        // string str;
+        // for (char c : s) {
+        //     if (isalnum(c))
+        //         str += tolower(c);
+        // }
+        // string temp = str;
+        // reverse(temp.begin(), temp.end());
+        // return str == temp;
+
+        //Vector Approach (100% Efficient)
+        // vector<char> filt;
+        // for (char c : s) {
+        //     if (isalnum(c))
+        //         filt.push_back(tolower(c));
+        // }
+        // vector<char> reversed(filt.rbegin(), filt.rend());
+        // return filt == reversed;
+    }
+};
