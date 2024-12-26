a3155548c423b19a4481e12bbe70a321e6fe2e57
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-27 01:02:02 +0500

    Question 11 added

---
+++ Two Pointers/Two Integer Sum II.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        // Two pointers Approach
+        // int n = numbers.size();
+        // vector<int> ans(2);
+        // int l = 0, r = n - 1;
+        // for (int i=0; i<n; i++) {
+        //     if ((numbers[l] + numbers[r]) == target){
+        //         ans[0] = l + 1; ans[1] = r + 1;
+        //         break;
+        //     }
+        //     else if ((numbers[l] + numbers[r]) > target)
+        //         r--;
+        //     else 
+        //         l++;
+        // }
+        // return ans;
+
+        
+    }
+};
